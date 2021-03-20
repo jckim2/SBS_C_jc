bf636f59532f37add0600c6daee1ae145b6cf8d0
jckim2 <wncks0303@naver.com>
2021-03-21 02:42:07 +0900

    마름모 완성

--- 마름모.c
+++ 마름모.c
@@ -3,62 +3,120 @@
 #include <stdio.h>
 
 #pragma warning (disable: 4996)
-void print_rhombus1(int m)
-{
 
-}
-void print_rhombus(int cnt)
+
+void print_rhombus_down(int cnt)
 {
+    int a;
+    int b;
+    int f = 2;
+    int c = 0;
+    int h = cnt;
+
+
+
+    for (a = 1; a <= cnt; a++)
     {
-        int a;
-        int c;
-        int cnt = 5;
-        int b = 1;
-        for (a = 1; a <= cnt; a++)
+        for (b = 1; b < f; b++)
         {
-            if (b >= cnt && (a == 2))
+            printf(" ");
+        }
+        if (cnt == 2)
+        {
+            c++;
+        }
+        if (cnt == 3)
+        {
+            if (a == 1)
             {
-                if (cnt == 5)
-                {
-                    b = b - 3;
-                }
-                if (cnt == 3)
-                {
-                    b = b - 1;
-                }
+                h++;
             }
-            if (b >= cnt && (a == 3))
+        }
+        if (cnt == 4)
+        {
+            if (a == 1)
             {
-                if (cnt == 5)
-                {
-                    b = b - 2;
-                }
+                h = h + 2;
+            }
+        }
+        if (cnt == 5)
+        {
+            if (a == 1)
+            {
+                h = h + 3;
+            }
+        }
 
+        for (c = h; c > a * 2 - 1; c--)
+        {
+            printf("*");
+        }
+        f++;
+        printf("\n");
+    }
+    printf("\n\n");
+}
+void print_rhombus_up(int cnt)
+{
+    int a, c;
+    int b = 1;
+    for (a = 1; a <= cnt; a++)
+    {
+        if (b >= cnt && (a == 2))
+        {
+            if (cnt == 5)
+            {
+                b = b - 3;
             }
-            if (b >= cnt && (a == 4))
+            if (cnt == 3)
+            {
+                b = b - 1;
+            }
+            if (cnt == 4)
+            {
+                b = b - 2;
+            }
+        }
+        if (b >= cnt && (a == 3))
+        {
+            if (cnt == 5)
+            {
+                b = b - 2;
+            }
+            if (cnt == 4)
+            {
+                b = b - 1;
+            }
+
+        }
+        if (b >= cnt && (a == 4))
+        {
+            if (cnt != 4)
             {
                 b--;
             }
-            for (b; b < cnt; b++)
-            {
-                printf(" ");
-            }
-            for (c = 0; c < a * 2 - 1; c++)
-            {
-                printf("*");
-            }
-            printf("\n");
         }
+        for (b; b < cnt; b++)
+        {
+            printf(" ");
+        }
+        for (c = 0; c < a * 2 - 1; c++)
+        {
+            printf("*");
+        }
+        printf("\n");
     }
 }
 int main(void) {
-    print_rhombus(1);
+    print_rhombus_up(1);
+    print_rhombus_down(1);
     // 출력
     /*
     *
     */
 
-    print_rhombus(2);
+    print_rhombus_up(2);
+    print_rhombus_down(2);
     // 출력
     /*
      *
@@ -66,7 +124,8 @@ int main(void) {
      *
     */
 
-    print_rhombus(3);
+    print_rhombus_up(3);
+    print_rhombus_down(3);
     // 출력
     /*
       *
@@ -75,8 +134,11 @@ int main(void) {
      ***
       *
     */
+    print_rhombus_up(4);
+    print_rhombus_down(4);
 
-    print_rhombus(5);
+    print_rhombus_up(5);
+    print_rhombus_down(5);
     // 출력
     /*
         *
